16afa915c8279388459798d5dca82595ab8e8c1b
Sam Sharing <Sam3258@users.noreply.github.com>
2020-09-03 13:27:30 +0800

    Update TC4_Tasks.ino

--- UnoTC4HeliOS/TC4_Tasks.ino
+++ UnoTC4HeliOS/TC4_Tasks.ino
@@ -23,61 +23,61 @@ float averageTemperature ( float *);
 void taskCmdHandle(int id_) {
 
     if ( Serial.available() ) {
-        String msg = Serial.readStringUntil('\n');
+		String msg = Serial.readStringUntil('\n');
 
 #if PRINT_ARTISAN_WHOLE_MESSAGE
-		SerialDebug.println(msg);                 // print whole message
+		SerialDebug.println(msg);                 			// print whole message
 #endif
 
-		if (msg.indexOf("READ")==0) {	            // READ command
+		if (msg.indexOf("READ")==0) {	            		// READ command
 /*
 	The READ command requests current temperature readings on all active channels. 
 	Response from TC4 device is ambient temperature followed by a comma separated list of temperatures in current active units
 	The logical channel order is : ambient,chan1,chan2,chan3,chan4
 */	
-			Serial.print("0.00,");			            // ambient temperature
+			Serial.print("0.00,");			      			// ambient temperature
 #ifdef ENVIRONMENT_TEMPERATURE_EXIST    
-      if (unit_C)  
-			  Serial.print(ET_CurTemp);			        // channel 1 : Environment Temperature (ET) with degree Celsius
-      else 
-      	Serial.print(ET_CurTemp * 9.0/5.0 + 32);			          // channel 1 : Environment Temperature (ET) with degree Farenheit
+      		if (unit_C)  
+				Serial.print(ET_CurTemp);	      			// channel 1 : Environment Temperature (ET) with degree Celsius
+      		else 
+      			Serial.print(ET_CurTemp * 9.0/5.0 + 32);	// channel 1 : Environment Temperature (ET) with degree Farenheit
 #else
-      if (unit_C)
-			  Serial.print(BT_AvgTemp);			        // channel 1 : Environment Temperature (ET); no ET sensor, so uses BT instead	
-      else
-        Serial.print(BT_AvgTemp * 9.0/5.0 + 32);			          // channel 1 : Environment Temperature (ET); no ET sensor, so uses BT instead	
+      		if (unit_C)
+			 	Serial.print(BT_AvgTemp);			        // channel 1 : Environment Temperature (ET); no ET sensor, so uses BT instead	
+      		else
+        		Serial.print(BT_AvgTemp * 9.0/5.0 + 32);    // channel 1 : Environment Temperature (ET); no ET sensor, so uses BT instead	
 #endif      				 
-    	Serial.print(",");		
-      if (unit_C)		
-			  Serial.print(BT_AvgTemp);	            // channel 2 : Bean Temperature (BT) with degree Celsius
-      else 
-        Serial.print(BT_AvgTemp * 9.0/5.0 + 32);	              // channel 2 : Bean Temperature (BT) with degree Farenheit 
-    	Serial.println(",0.00,0.00");	          // channel 3,4 : A vaule of zero indicates the channel is inactive
+    		Serial.print(",");		
+      		if (unit_C)		
+			  	Serial.print(BT_AvgTemp);	            	// channel 2 : Bean Temperature (BT) with degree Celsius
+      		else 
+        		Serial.print(BT_AvgTemp * 9.0/5.0 + 32);	// channel 2 : Bean Temperature (BT) with degree Farenheit 
+    		Serial.println(",0.00,0.00");	          		// channel 3,4 : A vaule of zero indicates the channel is inactive
 			
 // The READ command be sent from Artisan every 3 seconds (set by sample rate), unmark below code carefully
-//		SerialDebug.println("Artisan \"READ\"");						
-    } else if (msg.indexOf("UNITS;")== 0) {	  // UNIT command 
-      if (msg.substring(6,7)=="F") {   
-			  unit_C = false;
-        Serial.println("#OK Farenheit");
+//			SerialDebug.println("Artisan \"READ\"");						
+    	} else if (msg.indexOf("UNITS;")== 0) {	  			// UNIT command 
+      		if (msg.substring(6,7)=="F") {   
+			  	unit_C = false;
+        		Serial.println("#OK Farenheit");
 				SerialDebug.println("Artisan \"Farenheit\"");
-      }
-      else if (msg.substring(6,7)=="C") {  
-        unit_C = true;
-        Serial.println("#OK Celsius");
-			  SerialDebug.println("Artisan \"Celsius\"");
-      }
-    } else if (msg.indexOf("CHAN;")== 0) {    // CHAN command
-      Serial.print("#OK");
-	    SerialDebug.println("Artisan \"CHAN\"");
-    } else if (msg.indexOf("FILT;")== 0) {    // FILT command
-      Serial.print("#OK");
+      		}
+      		else if (msg.substring(6,7)=="C") {  
+        		unit_C = true;
+        		Serial.println("#OK Celsius");
+			  	SerialDebug.println("Artisan \"Celsius\"");
+      		}
+    	} else if (msg.indexOf("CHAN;")== 0) {    			// CHAN command
+      		Serial.print("#OK");
+	    	SerialDebug.println("Artisan \"CHAN\"");
+    	} else if (msg.indexOf("FILT;")== 0) {    			// FILT command
+      		Serial.print("#OK");
 			SerialDebug.println("Artisan \"FILT\"");
 		} else {
-		  SerialDebug.println("Artisan Unhandle command");
+		  	SerialDebug.println("Artisan Unhandle command");
 			SerialDebug.println(msg);
 		}
-  }
+  	}
 
 #if SEGMENT_DISPLAY_EXIST
     // Must run repeatedly; don't use blocking code (ex: delay()) in the loop() function or this won't work right
@@ -94,45 +94,45 @@ void taskThermo(int id_) {
     // read BT from MAX6675 thermal couple
     BT_CurTemp = BT_Thermocouple.readTempC();
 
-	  if ( isReady ) {
-        // means, first round of temperature array is done, can do averaging and filter out operation  
-	      BT_AvgTemp = averageTemperature( &BT_TempArray[0] );
+	if ( isReady ) {
+    	// means, first round of temperature array is done, can do averaging and filter out operation  
+	    BT_AvgTemp = averageTemperature( &BT_TempArray[0] );
 
         // filter out abnormal temperature-up only, bypass temperature-down
-	      // because "CHARGE" period, the temperature-down may large than 10 degree
-	      if ( BT_CurTemp < (BT_AvgTemp + ABNORMAL_TEMPERATURE_DEGREE) ) {
-	          // temperature is in-arrange, store it
-	    	    BT_TempArray[BT_ArrayIndex] = BT_CurTemp;
-	      }
-	      else {
-	          // set abnormal flag
-	    	    abnormalValue = true;
-	    	    // print ? with temperature value in newline
+	    // because "CHARGE" period, the temperature-down may large than 10 degree
+	    if ( BT_CurTemp < (BT_AvgTemp + ABNORMAL_TEMPERATURE_DEGREE) ) {
+	    	// temperature is in-arrange, store it
+	    	BT_TempArray[BT_ArrayIndex] = BT_CurTemp;
+	     }
+	     else {
+	     	// set abnormal flag
+	    	abnormalValue = true;
+	    	// print ? with temperature value in newline
             SerialDebug.println(" ");
             SerialDebug.print(" ?");
             SerialDebug.println(BT_CurTemp);
-	      }
-	  }
-	  else {
+	   	}
+	}
+	else {
 	   	// just read current temperature
 	   	BT_TempArray[BT_ArrayIndex] = BT_CurTemp;
-	  }
+	}
 
     if ( !abnormalValue ) {
-        // Normal temperature will into this loop
+    	// Normal temperature will into this loop
 #if PRINT_TEAMPERATURE_EACH_READING
         // print MAX6675 reading value on serial monitor
-	   	  if ( BT_ArrayIndex == 0 ) {
-			      SerialDebug.println(" ");
-	   		    SerialDebug.print("Temperature: ");
-	   	  }
+	   	if ( BT_ArrayIndex == 0 ) {
+			SerialDebug.println(" ");
+	   		SerialDebug.print("Temperature: ");
+	  	}
 
         SerialDebug.print(" ");			
-	   	  SerialDebug.print(BT_CurTemp);
+	   	SerialDebug.print(BT_CurTemp);
 #endif
         BT_ArrayIndex++;
         if ( BT_ArrayIndex >= TEMPERATURE_ARRAY_LENGTH ) {
-            BT_ArrayIndex = 0;
+        	BT_ArrayIndex = 0;
 
             if ( !isReady ) {
                 isReady = true;
@@ -166,26 +166,26 @@ void taskThermo(int id_) {
 // A function to average temperature array which values are from MAX6675 temperature reading
 float averageTemperature ( float *pTemp ) {
 
-	  float avg = *pTemp;
+	float avg = *pTemp;
 
 #if 0 
     // Below code works on ESP32 and ESP8266 but get OVF error on UNO,
     // I think this is because UNO is 8-bit microcontroller
     pTemp++;
     for (int i=0; i<=(TEMPERATURE_ARRAY_LENGTH-1); i++) {
-		  avg = avg + *pTemp;
-      avg =  avg / 2;
-      pTemp++;
+		avg = avg + *pTemp;
+      	avg =  avg / 2;
+      	pTemp++;
 }
 #else
     pTemp++;
-	  avg = avg + *pTemp;
+	avg = avg + *pTemp;
     avg =  avg / 2;
     pTemp++;
-	  avg = avg + *pTemp;
+	avg = avg + *pTemp;
     avg =  avg / 2;
     pTemp++;
-	  avg = avg + *pTemp;
+	avg = avg + *pTemp;
     avg =  avg / 2;
 #endif  
 
